2e99c83b19140aad213c51a3a15919bd0d30d2e0
whweve <43516477+whweve@users.noreply.github.com>
2024-01-08 16:13:57 +0800

    Update IntGenicPlot.R
    add exon size contrl

--- R/IntGenicPlot.R
+++ R/IntGenicPlot.R
@@ -38,6 +38,7 @@
 #' @param thresholdlinecolour colour of threshold line, default gray.
 #' @param upperpointsize size of point of association sites, default 1.
 #' @param utrsize size of utr, default 4.
+#' @param exonsize size of exon, default 4.
 #' @return ggplot2 plot
 #' @export
 #' @import ggplot2 SNPRelate reshape2 gdsfmt ggrepel
@@ -58,7 +59,7 @@ IntGenicPlot <- function(transcript, gtf, association, hapmap, hapmap_ld = NULL,
                          colour02 = "gray", colour04 = "cyan", colour06 = "green", colour08 = "yellow", 
                          colour10 = "red", leadsnp_shape = 23, leadsnp_colour = "black", leadsnp_fill = "purple", 
                          leadsnp_size = 1.5, marker2highlight = NULL, marker2label = NULL, marker2label_angle = 60, 
-                         marker2label_size = 1,thresholdlinecolour="gray",upperpointsize=1,utrsize=4) {
+                         marker2label_size = 1,thresholdlinecolour="gray",upperpointsize=1,utrsize=4,exonsize=4) {
   if (sum(grepl(transcript, gtf$V9)) == 0) {
     stop("please provide the correct transcript or the gtf file")
   } else {
@@ -128,6 +129,19 @@ IntGenicPlot <- function(transcript, gtf, association, hapmap, hapmap_ld = NULL,
                                                                                                        struct)), aes(x = V4, xend = V5, y = -max(pvalue_range) * fold/30, 
                                                                                                                      yend = -max(pvalue_range) * fold/30), colour = get(paste0(struct, 
                                                                                                                                                                                "_colour")), size = 4)))
+        } else {
+          assign(paste0("transcript_structure_", struct, "_list"), NULL)
+        }
+      }
+      #add exon size control, some part of exon may be utr
+      for (struct in c("cds", "exon")) {
+        assign(paste0("transcript_structure_", struct), transcript_corrdination[grep(struct, 
+                                                                                     transcript_corrdination$V3, ignore.case = TRUE), ])
+        if (dim(get(paste0("transcript_structure_", struct)))[1] > 0) {
+          assign(paste0("transcript_structure_", struct, "_list"), list(geom_segment(data = get(paste0("transcript_structure_", 
+                                                                                                       struct)), aes(x = V4, xend = V5, y = -max(pvalue_range) * fold/30, 
+                                                                                                                     yend = -max(pvalue_range) * fold/30), colour = get(paste0(struct, 
+                                                                                                                                                                               "_colour")), size = exonsize)))
         } else {
           assign(paste0("transcript_structure_", struct, "_list"), NULL)
         }
